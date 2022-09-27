793e1c9cddca60e3e2bfe8a1747f436415435f5a
Alexdelia <alexandre.delille.57@gmail.com>
2022-09-27 15:43:01 +0200

    improve

--- Python_Module_00/ex04/operation.py
+++ Python_Module_00/ex04/operation.py
@@ -8,7 +8,7 @@ if __name__ != "__main__":
 if len(sys.argv) == 1:
     print("usage:\t\033[1m" + __file__, "\033[35m<number>\033[0m")
     sys.exit()
-elif len(sys.argv) > 2:
+elif len(sys.argv) != 3:
     print(
         f"\033[1;31mAssertionError:\033[35m\t{len(sys.argv) - 1}\033[0m \033[31marguments provided, expected \033[1;35m1\033[0m")
     sys.exit()
