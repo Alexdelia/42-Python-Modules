a7cc1ce5004a627896a95f85004251fd1b6fde43
Alexdelia <alexandre.delille.57@gmail.com>
2022-09-27 15:53:44 +0200

    working, will code tests

--- Python_Module_00/ex04/operation.py
+++ Python_Module_00/ex04/operation.py
@@ -6,7 +6,7 @@ if __name__ != "__main__":
     sys.exit()
 
 if len(sys.argv) == 1:
-    print("usage:\t\033[1m" + __file__, "\033[35m<number>\033[0m")
+    print("usage:\t\033[1m" + __file__, "\033[35m<number1> <number2>\033[0m")
     sys.exit()
 elif len(sys.argv) != 3:
     print(
@@ -14,10 +14,28 @@ elif len(sys.argv) != 3:
     sys.exit()
 
 try:
-    n = int(sys.argv[1])
+    x = int(sys.argv[1])
 except ValueError:
     print(
         f"\033[1;31mAssertionError:\033[35m\t{sys.argv[1]}\033[0m \033[31mis not an integer\033[0m")
     sys.exit()
+try:
+    y = int(sys.argv[2])
+except ValueError:
+    print(
+        f"\033[1;31mAssertionError:\033[35m\t{sys.argv[2]}\033[0m \033[31mis not an integer\033[0m")
+    sys.exit()
 
-print("I'm", [["Even", "Odd"][n % 2], "Zero"][n == 0] + ".")
+print(f"\033[1mSum:\033[0m\t\t\033[1;35m{x + y}\033[0m")
+print(f"\033[1mDifference:\033[0m\t\033[1;35m{x - y}\033[0m")
+print(f"\033[1mProduct:\033[0m\t\033[1;35m{x * y}\033[0m")
+print("\033[1mQuotient:\033[0m\t", end="")
+try:
+    print(f"\033[1;35m{x / y}\033[0m")
+except ZeroDivisionError:
+    print("\033[1;31mERROR\033[0m\t\033[31m(division by zero)\033[0m")
+print("\033[1mRemainder:\033[0m\t", end="")
+try:
+    print(f"\033[1;35m{x % y}\033[0m")
+except ZeroDivisionError:
+    print("\033[1;31mERROR\033[0m\t\033[31m(modulo by zero)\033[0m")
