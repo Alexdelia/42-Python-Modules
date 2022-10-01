c3622bd1f605937aaa7a0d9f2b01ad6594708760
Alexdelia <alexandre.delille.57@gmail.com>
2022-10-01 13:47:52 +0200

    Saving

--- Python_Module_00/ex10/loading.py
+++ Python_Module_00/ex10/loading.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+from math import ceil
+from os import get_terminal_size
 from time import time
+from typing import Any, Generator
 
+_w = get_terminal_size().columns - 64
 
-def ft_progress(listy: list, w: int = 40) -> list:
+def ft_progress(listy: 'list[Any]', w: int = _w) -> Generator[Any, None, None]:
     """
     This function displays a progress bar like this:
     ETA: 8.67s [ 23%][=====> ] 233/1000 | elapsed time 2.33s
@@ -12,14 +16,14 @@ def ft_progress(listy: list, w: int = 40) -> list:
     start = time()
     for i, item in enumerate(listy):
         percent = i / size * 100
-        perw = int(i / size * w)
+        perw = ceil(i / size * w)
         elapsed = time() - start
         eta = elapsed * size / (i + 1) - elapsed
         print(f"\r\033[1;34mETA: {eta:.2f}s\033[0m",
               f"\t[{percent:3.0f}%]",
-              f"[{'=' * perw}>{' ' * (w - perw)}]",
+              f"[{'=' * perw}{['>','='][i == size - 1]}{' ' * (w - perw)}]",
               f"{i + 1}/{size}",
-              f"| elapsed time {elapsed:.2f}s",
+              f"\t| elapsed time {elapsed:.2f}s",
               end="")
         yield item
 
--- Python_Module_00/ex10/main.py
+++ Python_Module_00/ex10/main.py
@@ -2,12 +2,27 @@
 
 import sys
 from time import sleep
+from typing import Callable
 
 from loading import ft_progress
 
 if __name__ != "__main__":
     sys.exit()
 
+
+def test(name: str, listy: range, f: Callable[[int], int], sleep_time: float = 0.01) -> None:
+    """
+    This function tests the ft_progress function.
+    """
+    print(f">> \033[1;3;36mTEST {name}\033[0m")
+    ret = 0
+    for item in ft_progress(listy):
+        ret += f(item)
+        sleep(sleep_time)
+    print(f"\n>> \033[1;3;32m{ret}\033[0m\n")
+
+listy = range(1000)
+test("0", listy, lambda x: (x + 3) % 5, 0.01)
 print("TEST 0")
 listy = range(1000)
 ret = 0
