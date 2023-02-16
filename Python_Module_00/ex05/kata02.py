21a64cfc2e7074005eb7403d05e430027cb22468
Alexdelia <alexandre.delille.57@gmail.com>
2023-02-16 18:22:42 +0100

    `flake8`

--- Python_Module_00/ex05/kata02.py
+++ Python_Module_00/ex05/kata02.py
@@ -8,5 +8,5 @@ if __name__ != "__main__":
 kata = (2019, 9, 25, 3, 30)
 
 print(f"{kata[1]:02d}/{kata[2]:02d}/{kata[0]:04d} {kata[3]:02d}:{kata[4]:02d}")
-#print("%02d/%02d/%04d %02d:%02d" % (kata[1], kata[2], kata[0], kata[3], kata[4]))
-#print("{:02d}/{:02d}/{:04d} {:02d}:{:02d}".format(kata[1], kata[2], kata[0], kata[3], kata[4]))
+# print("%02d/%02d/%04d %02d:%02d" % (kata[1], kata[2], kata[0], kata[3], kata[4]))
+# print("{:02d}/{:02d}/{:04d} {:02d}:{:02d}".format(kata[1], kata[2], kata[0], kata[3], kata[4]))
