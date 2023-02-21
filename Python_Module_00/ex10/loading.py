1229a05110e43f4e3453ed14dd4e3590803b1a37
Alexdelia <alexandre.delille.57@gmail.com>
2023-02-21 19:50:28 +0100

    `flake8`

--- Python_Module_00/ex10/loading.py
+++ Python_Module_00/ex10/loading.py
@@ -3,17 +3,27 @@
 from math import ceil
 from os import get_terminal_size
 from time import time
-from typing import Any, Generator
+from typing import Generator, Iterable, TypeVar
 
 _w = get_terminal_size().columns - 72
 if _w < 0:
     _w = 3
 
-def ft_progress(listy: 'list[Any]', w: int = _w, fill: str = '=', tip: str = '>') -> Generator[Any, None, None]:
+T = TypeVar('T')
+
+
+def ft_progress(
+    listy: Iterable[T],
+    w: int = _w,
+    fill: str = '=',
+    tip: str = '>',
+) -> Generator[T, None, None]:
     """
     This function displays a progress bar like this:
     ETA: 8.67s [ 23%][=====> ] 233/1000 | elapsed time 2.33s
     """
+    listy = list(listy)
+
     try:
         size = len(listy)
     except TypeError:
@@ -27,13 +37,21 @@ def ft_progress(listy: 'list[Any]', w: int = _w, fill: str = '=', tip: str = '>'
         perw = ceil(i / (size - 1) * w)
         elapsed = time() - start
         eta = elapsed * size / (i + 1) - elapsed
-        print(f"\r\033[1;2;34mETA:\033[0m \033[1;3;34m{eta:.2f}s\033[0m",
-              f"\t\033[1m[\033[32m{percent:3.0f}%\033[0m\033[1m]",
-              f"[\033[35m{fill * perw}{[tip,fill][i == size - 1]}\033[0m{' ' * (w - perw)}\033[1m]",
-              f"\033[3m{i + 1}\033[0m\033[1m/\033[2;3m{size}\033[0m",
-              f"\t\033[1m| \033[2;33melapsed time \033[0m\033[1;3;33m{elapsed:.2f}s",
-              end="\033[0m")
+        print(
+            f"\r\033[1;2;34mETA:\033[0m \033[1;3;34m{eta:.2f}s\033[0m",
+            f"\t\033[1m[\033[32m{percent:3.0f}%\033[0m\033[1m]",
+            f"[\033[35m{fill * perw}{[tip,fill][i == size - 1]}\
+\033[0m{' ' * (w - perw)}\033[1m]",
+            f"\033[3m{i + 1}\033[0m\033[1m/\033[2;3m{size}\033[0m",
+            f"\t\033[1m| \033[2;33melapsed time\
+ \033[0m\033[1;3;33m{elapsed:.2f}s",
+            end="\033[0m"
+        )
         yield item
 
+
 if __name__ == "__main__":
-    print(f"{__file__} only contain the function ft_progress:\n{ft_progress.__doc__}")
+    print(
+        f"{__file__} only contain the function",
+        f"ft_progress:\n{ft_progress.__doc__}"
+    )
--- Python_Module_00/ex10/main.py
+++ Python_Module_00/ex10/main.py
@@ -10,7 +10,12 @@ if __name__ != "__main__":
     sys.exit()
 
 
-def test(name: str, listy: range, f: Callable[[int], int], sleep_time: float = 0.01) -> None:
+def test(
+    name: str,
+    listy: range,
+    f: Callable[[int], int],
+    sleep_time: float = 0.01,
+) -> None:
     """
     This function tests the ft_progress function.
     """
@@ -20,11 +25,19 @@ def test(name: str, listy: range, f: Callable[[int], int], sleep_time: float = 0
         if type(item) is int:
             ret += f(item)
         sleep(sleep_time)
-    print("\n>>\033[1;3;32m",
-          [f"{ret}", f"{ret:e}"][ret > 100000000],
-          end="\033[0m\n\n")
+    print(
+        "\n>>\033[1;3;32m", [f"{ret}", f"{ret:e}"][ret > 100000000],
+        end="\033[0m\n\n"
+    )
 
-def test_w(name: str, listy: range, f: Callable[[int], int], w: int, sleep_time: float = 0.01) -> None:
+
+def test_w(
+    name: str,
+    listy: range,
+    f: Callable[[int], int],
+    w: int,
+    sleep_time: float = 0.01,
+) -> None:
     """
     This function tests the ft_progress function with a custom width.
     """
@@ -35,16 +48,30 @@ def test_w(name: str, listy: range, f: Callable[[int], int], w: int, sleep_time:
         sleep(sleep_time)
     print(f"\n>> \033[1;3;32m{ret}\033[0m\n")
 
-def test_w_fill_tip(name: str, listy: range, f: Callable[[int], int], w: int, fill: str, tip: str, sleep_time: float = 0.01) -> None:
+
+def test_w_fill_tip(
+    name: str,
+    listy: range,
+    f: Callable[[int], int],
+    w: int,
+    fill: str,
+    tip: str,
+    sleep_time: float = 0.01,
+) -> None:
     """
-    This function tests the ft_progress function with a custom width, fill and tip.
+    This function tests the ft_progress\
+function with a custom width, fill and tip.
     """
-    print(f">> \033[1;3;36mTEST {name} (width: {w}, fill '{fill}', tip: '{tip}')\033[0m")
+    print(
+        f">> \033[1;3;36mTEST {name}",
+        f"(width: {w}, fill '{fill}', tip: '{tip}')\033[0m"
+    )
     ret = 0
     for item in ft_progress(listy, w, fill, tip):
         ret += f(item)
         sleep(sleep_time)
     print(f"\n>> \033[1;3;32m{ret}\033[0m\n")
+
 
 test("0", range(1000), lambda x: (x + 3) % 5, 0.01)
 test("1", range(3333), lambda x: x, 0.005)
@@ -56,12 +83,12 @@ test("6 (range(0))", range(0), lambda x: x, 1)
 test("7 (range(1))", range(1), lambda x: x, 1)
 test("8 (range(2))", range(2), lambda x: x, 1)
 test("9 (range(3))", range(3), lambda x: x, 1)
-test("10 ([])", [], lambda x: x, 1)
+test("10 ([])", [], lambda x: x, 1)  # type: ignore
 try:
-    test("11 (None)", None, lambda x: x, 1)
+    test("11 (None)", None, lambda x: x, 1)  # type: ignore
 except TypeError:
     print(">> \033[1;3;31mTypeError\t[KO]\033[0m\n")
-test("12 (str)", "Hello World!", lambda x: 0, 0.1)
-test("13 (x**x + sleep(0)", range(100), lambda x: x**x, 0)
+test("12 (str)", "Hello World!", lambda x: 0, 0.1)  # type: ignore
+test("13 (x**x + sleep(0)", range(100), lambda x: x ** x, 0)
 test_w_fill_tip("14", range(1000), lambda x: x, 10, '>', '=')
 test_w_fill_tip("15", range(1000), lambda x: x, 10, '█', ' ')
