5cfb29fef1af3f908b1d5e2ed36c9e31c26436c3
Alexdelia <alexandre.delille.57@gmail.com>
2023-02-25 12:20:21 +0100

    continuing Vector

---
+++ Python_Module_01/ex02/test.py
@@ -0,0 +1,343 @@
+#!/usr/bin/env python3
+
+import pytest
+from vector import Vector
+
+if __name__ == "__main__":
+    pytest.main(args=["-v", __file__])
+
+
+@pytest.fixture(scope="function")
+def values_column():
+    return [[0.0], [1.0], [2.0], [3.0]]
+
+
+@pytest.fixture(scope="function")
+def values_row():
+    return [[0.0, 1.0, 2.0, 3.0]]
+
+
+@pytest.fixture(scope="function")
+def shape_column():
+    return (4, 1)
+
+
+@pytest.fixture(scope="function")
+def shape_row():
+    return (1, 4)
+
+
+def test_vector_basic(values_column, values_row, shape_column, shape_row):
+    v = Vector(values_column)
+    assert v.values == values_column
+    assert v.shape == shape_column
+    assert str(v) == "Vector([[0.0], [1.0], [2.0], [3.0]])"
+    assert repr(v) == "Vector([[0.0], [1.0], [2.0], [3.0]])"
+
+    v = Vector(values_row)
+    assert v.values == values_row
+    assert v.shape == shape_row
+    assert str(v) == "Vector([[0.0, 1.0, 2.0, 3.0]])"
+    assert repr(v) == "Vector([[0.0, 1.0, 2.0, 3.0]])"
+
+    neg = [[-v for v in values_row[0]]]
+    v = Vector(neg)
+    assert v.values == neg
+    assert v.shape == shape_row
+
+
+def test_vector_basic_invalid():
+    with pytest.raises(ValueError):
+        Vector([[0.0, 1.0], [2.0, 3.0, 4.0]])
+
+    with pytest.raises(ValueError):
+        Vector([[]])
+
+    with pytest.raises((TypeError, ValueError)):
+        Vector([])
+
+    with pytest.raises(TypeError):
+        Vector([[[0.0]]])  # type: ignore
+
+    with pytest.raises(ValueError):
+        Vector([[0.0, 1.0], [2.0]])
+
+    with pytest.raises(TypeError):
+        Vector([[0, 1.0]])  # type: ignore
+
+    with pytest.raises(TypeError):
+        Vector([["0", 1.0]])  # type: ignore
+
+    with pytest.raises(ValueError):
+        Vector([[0.0], [], [2.0]])
+
+    with pytest.raises(TypeError):
+        Vector()  # type: ignore
+
+
+def test_vector_size():
+    v = Vector(5)
+    assert v.values == [[0.0], [1.0], [2.0], [3.0], [4.0]]
+    assert v.shape == (5, 1)
+
+    v = Vector(1)
+    assert v.values == [[0.0]]
+    assert v.shape == (1, 1)
+
+
+def test_vector_size_invalid():
+    with pytest.raises(ValueError):
+        Vector(0)
+
+    with pytest.raises(ValueError):
+        Vector(-1)
+
+    with pytest.raises(TypeError):
+        Vector("1")  # type: ignore
+
+    with pytest.raises(TypeError):
+        Vector(1.0)  # type: ignore
+
+    with pytest.raises(TypeError):
+        Vector(None)  # type: ignore
+
+
+def test_vector_range():
+    v = Vector((10, 16))
+    assert v.values == [[10.0], [11.0], [12.0], [13.0], [14.0], [15.0]]
+    assert v.shape == (6, 1)
+
+    v = Vector((1, 2))
+    assert v.values == [[1.0]]
+    assert v.shape == (1, 1)
+
+
+def test_vector_range_invalid():
+    with pytest.raises(ValueError):
+        Vector((1, 0))
+
+    with pytest.raises(ValueError):
+        Vector((1, 1))
+
+    with pytest.raises(TypeError):
+        Vector((1, "1"))  # type: ignore
+
+    with pytest.raises(TypeError):
+        Vector((1, 1.0))  # type: ignore
+
+    with pytest.raises(TypeError):
+        Vector((1, None))  # type: ignore
+
+    with pytest.raises((TypeError, ValueError)):
+        Vector((1, 1, 1))  # type: ignore
+
+
+def test_vector_transpose(values_column, values_row, shape_column, shape_row):
+    v = Vector(values_column)
+    assert v.T().values == values_row
+    assert v.T().shape == shape_row
+
+    v = Vector(values_row)
+    assert v.T().values == values_column
+    assert v.T().shape == shape_column
+
+
+def test_vector_dot(values_column, values_row):
+    v1 = Vector(values_column)
+    v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+
+    assert v1.dot(v2) == 18.0
+
+    v1 = Vector(values_row)
+    v2 = Vector([[2.0, 1.5, 2.25, 4.0]])
+
+    assert v1.dot(v2) == 18.0
+
+    v1 = Vector([[1.0, 3.0]])
+    v2 = Vector([[2.0, 4.0]])
+
+    assert v1.dot(v2) == 14.0
+
+    v1 = Vector([[1.0], [3.0]])
+    v2 = Vector([[2.0], [4.0]])
+
+    assert v1.dot(v2) == 14.0
+
+
+def test_vector_dot_invalid(values_column, values_row):
+    v1 = Vector(values_column)
+    v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+
+    with pytest.raises(ValueError):
+        v1.dot(v2.T())
+
+    with pytest.raises(ValueError):
+        v1.T().dot(v2)
+
+    assert v1.T().dot(v2.T()) == 18.0
+
+    v1 = Vector(values_row)
+    v2 = Vector([[2.0, 1.5, 2.25, 4.0]])
+
+    with pytest.raises(ValueError):
+        v1.dot(v2.T())
+
+    with pytest.raises(ValueError):
+        v1.T().dot(v2)
+
+    assert v1.T().dot(v2.T()) == 18.0
+
+    with pytest.raises(TypeError):
+        v1.dot(1.0)  # type: ignore
+
+    with pytest.raises(TypeError):
+        v1.dot("1")  # type: ignore
+
+    with pytest.raises(TypeError):
+        v1.dot(1)  # type: ignore
+
+
+def test_vector_add(values_column, values_row, shape_column, shape_row):
+    v1 = Vector(values_column)
+    v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+
+    v3 = v1 + v2
+    assert v3.values == [[2.0], [2.5], [4.25], [7.0]]
+    assert v3.shape == shape_column
+
+    v3 = v2 + v1
+    assert v3.values == [[2.0], [2.5], [4.25], [7.0]]
+    assert v3.shape == shape_column
+
+    v1 = Vector(values_row)
+    v2 = Vector([[2.0, 1.5, 2.25, 4.0]])
+
+    v3 = v1 + v2
+    assert v3.values == [[2.0, 2.5, 4.25, 7.0]]
+    assert v3.shape == shape_row
+
+    v3 = v2 + v1
+    assert v3.values == [[2.0, 2.5, 4.25, 7.0]]
+    assert v3.shape == shape_row
+
+
+def test_vector_add_invalid(values_column, values_row):
+    v1 = Vector(values_column)
+    v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+
+    with pytest.raises(ValueError):
+        _ = v1 + v2.T()
+
+    with pytest.raises(ValueError):
+        _ = v1.T() + v2
+
+    v1 = Vector(values_row)
+    v2 = Vector([[2.0, 1.5, 2.25, 4.0]])
+
+    with pytest.raises(ValueError):
+        _ = v1 + v2.T()
+
+    with pytest.raises(ValueError):
+        _ = v1.T() + v2
+
+    with pytest.raises(TypeError):
+        v1 + 1.0  # type: ignore
+
+    with pytest.raises(TypeError):
+        v1 + "1"  # type: ignore
+
+    with pytest.raises(TypeError):
+        v1 + 1  # type: ignore
+
+
+def test_vector_sub(values_column, values_row, shape_column, shape_row):
+    v1 = Vector(values_column)
+    v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+
+    v3 = v1 - v2
+    assert v3.values == [[-2.0], [-0.5], [-0.25], [-1.0]]
+    assert v3.shape == shape_column
+
+    v3 = v2 - v1
+    assert v3.values == [[2.0], [0.5], [0.25], [1.0]]
+    assert v3.shape == shape_column
+
+    v1 = Vector(values_row)
+    v2 = Vector([[2.0, 1.5, 2.25, 4.0]])
+
+    v3 = v1 - v2
+    assert v3.values == [[-2.0, -0.5, -0.25, -1.0]]
+    assert v3.shape == shape_row
+
+    v3 = v2 - v1
+    assert v3.values == [[2.0, 0.5, 0.25, 1.0]]
+    assert v3.shape == shape_row
+
+
+def test_vector_sub_invalid(values_column, values_row):
+    v1 = Vector(values_column)
+    v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+
+    with pytest.raises(ValueError):
+        _ = v1 - v2.T()
+
+    with pytest.raises(ValueError):
+        _ = v1.T() - v2
+
+    v1 = Vector(values_row)
+    v2 = Vector([[2.0, 1.5, 2.25, 4.0]])
+
+    with pytest.raises(ValueError):
+        _ = v1 - v2.T()
+
+    with pytest.raises(ValueError):
+        _ = v1.T() - v2
+
+    with pytest.raises(TypeError):
+        v1 - 1.0  # type: ignore
+
+    with pytest.raises(TypeError):
+        v1 - "1"  # type: ignore
+
+    with pytest.raises(TypeError):
+        v1 - 1  # type: ignore
+
+
+def test_vector_mul(values_column, values_row, shape_column, shape_row):
+    v1 = Vector(values_column)
+    v2 = v1 * 5
+    assert v2.values == [[0.0], [5.0], [10.0], [15.0]]
+    assert v2.shape == shape_column
+
+    v1 = Vector(values_row)
+    v2 = v1 * 5
+    assert v2.values == [[0.0, 5.0, 10.0, 15.0]]
+
+
+def test_vector_mul_invalid(values_column, values_row):
+    v1 = Vector(values_column)
+    v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+
+    with pytest.raises(ValueError):
+        _ = v1 * v2.T()
+
+    with pytest.raises(ValueError):
+        _ = v1.T() * v2
+
+    v1 = Vector(values_row)
+    v2 = Vector([[2.0, 1.5, 2.25, 4.0]])
+
+    with pytest.raises(ValueError):
+        _ = v1 * v2.T()
+
+    with pytest.raises(ValueError):
+        _ = v1.T() * v2
+
+    with pytest.raises(TypeError):
+        v1 * 1.0  # type: ignore
+
+    with pytest.raises(TypeError):
+        v1 * "1"  # type: ignore
+
+    with pytest.raises(TypeError):
+        v1 * 1  # type: ignore
--- Python_Module_01/ex02/vector.py
+++ Python_Module_01/ex02/vector.py
@@ -1,11 +1,14 @@
+from __future__ import annotations
+
 from typing import Any, Optional, TypeAlias
 
 VectorValues: TypeAlias = list[list[float]]
+VectorShape: TypeAlias = tuple[int, int]
 
 
 class Vector:
 
-    def __init__(self, values: VectorValues | int | range):
+    def __init__(self, values: VectorValues | int | tuple[int, int]):
         if Vector._check_values_vector(values):
             assert isinstance(values, list), \
                 Vector._check_values_vector.__name__ \
@@ -17,23 +20,26 @@ class Vector:
                 + f" returned True but values: '{values}' is not a {int}"
             self.values = [[float(i)] for i in range(values)]
         elif Vector._check_values_range(values):
-            assert isinstance(values, range), \
+            assert isinstance(values, tuple), \
                 Vector._check_values_range.__name__ \
-                + f" returned True but values: '{values}' is not a {range}"
-            self.values = [[float(i)] for i in values]
+                + f" returned True but values: '{values}'" \
+                + f" is not a {VectorShape} (range)"
+            self.values = [[float(i)] for i in range(*values)]
         else:
-            raise ValueError(
+            raise TypeError(
                 f"Vector values must be {VectorValues}, int or range"
             )
 
     @staticmethod
     def _check_values_vector(values: Any) -> bool:
-        if not isinstance(values, list) or not isinstance(values[0], list):
+        if not isinstance(values, list) \
+                or not values \
+                or not isinstance(values[0], list):
             return False
 
         if Vector._check_column_vector(values):
             return True
-        if Vector._check_row_vector(values):
+        elif Vector._check_row_vector(values):
             return True
         else:
             raise ValueError("Vector cannot be empty")
@@ -64,7 +70,9 @@ class Vector:
 
     @staticmethod
     def _check_row_vector(values: list[list[Any]]) -> bool:
-        if len(values[0]) != 1:
+        if len(values) != 1:
+            return False
+        elif len(values[0]) < 1:
             return False
 
         for i, v in enumerate(values[0]):
@@ -85,13 +93,118 @@ class Vector:
 
     @staticmethod
     def _check_values_range(values: Any) -> bool:
-        if not isinstance(values, range):
+        if not isinstance(values, tuple):
             return False
 
-        if len(values) <= 0:
-            raise ValueError("Vector size must be greater than 0")
+        if len(values) != 2:
+            raise ValueError("Vector range must be a tuple of 2 elements")
+
+        for i, v in enumerate(values):
+            if not isinstance(v, int):
+                raise TypeError(_error(v, i, holder="values", t=int))
+
+        if values[0] >= values[1]:
+            raise ValueError(
+                f"{values[0]} must be less than {values[1]}\
+ for values to be a range"
+            )
 
         return True
+
+    @property
+    def shape(self) -> VectorShape:
+        return (len(self.values), len(self.values[0]))
+
+    def dot(self, other: Vector) -> float:
+        _is_type(other, Vector, op="dot product")
+
+        if self.shape != other.shape:
+            raise ValueError(_error_shape("dot", self.shape, other.shape))
+
+        if self.shape[0] == 1:
+            return sum(
+                [sv * ov for sv, ov in zip(self.values[0], other.values[0])]
+            )
+        else:
+            return sum(
+                [sv[0] * ov[0] for sv, ov in zip(self.values, other.values)]
+            )
+
+    def T(self) -> Vector:
+        # Transpose:
+        # [[1, 2, 3]] -> [[1], [2], [3]]    # row to column
+        # [[1], [2], [3]] -> [[1, 2, 3]]    # column to row
+        if self.shape[0] == 1:
+            return Vector([[v] for v in self.values[0]])
+        else:
+            return Vector([[v[0] for v in self.values]])
+
+    def __str__(self) -> str:
+        return f"Vector({self.values})"
+
+    def __repr__(self) -> str:
+        return f"Vector({self.values})"
+
+    def __add__(self, other: Vector) -> Vector:
+        _is_type(other, Vector, op="add")
+
+        if self.shape != other.shape:
+            raise ValueError(_error_shape("add", self.shape, other.shape))
+
+        if self.shape[0] == 1:
+            return Vector(
+                [[sv + ov for sv, ov in zip(self.values[0], other.values[0])]]
+            )
+        else:
+            return Vector(
+                [[sv[0] + ov[0]] for sv, ov in zip(self.values, other.values)]
+            )
+
+    def __radd__(self, other: Vector) -> Vector:
+        return self + other
+
+    def __sub__(self, other: Vector) -> Vector:
+        _is_type(other, Vector, op="sub")
+
+        if self.shape != other.shape:
+            raise ValueError(_error_shape("sub", self.shape, other.shape))
+
+        if self.shape[0] == 1:
+            return Vector(
+                [[sv - ov for sv, ov in zip(self.values[0], other.values[0])]]
+            )
+        else:
+            return Vector(
+                [[sv[0] - ov[0]] for sv, ov in zip(self.values, other.values)]
+            )
+
+    def __rsub__(self, other: Vector) -> Vector:
+        return self - other
+
+    def __truediv__(self, other: float | int) -> Vector:
+        _is_type(other, (float, int), op="truediv")
+
+        # automatic raise ZeroDivisionError
+        if self.shape[0] == 1:
+            return Vector([[sv / other for sv in self.values[0]]])
+        else:
+            return Vector([[sv[0] / other] for sv in self.values])
+
+    def __rtruediv__(self, other: float | int) -> Vector:
+        raise NotImplementedError(
+            "Division of a scalar by a Vector is not defined here."
+        )
+
+    def __mul__(self, other: float | int) -> Vector:
+        _is_type(other, (float, int), op="mul")
+
+        if self.shape[0] == 1:
+            return Vector([[sv * other for sv in self.values[0]]])
+        else:
+            return Vector([[sv[0] * other] for sv in self.values])
+
+    def __rmul__(self, other: float | int) -> Vector:
+        return self * other
 
 
 def _error(
@@ -105,3 +218,34 @@ def _error(
 {' in ' + holder if holder is not None else ''}\
 {' is not a ' + str(t) if t is not None else ''}\
 {' ' + msg if msg is not None else ''}"
+
+
+def _error_shape(
+    op: str,
+    s1: VectorShape,
+    s2: VectorShape,
+) -> str:
+    return f"cannot {op} vectors of different shapes: {s1} != {s2}"
+
+
+def _is_type(
+    v: object,
+    t: type | tuple[type, ...],
+    r: bool = True,
+    op: Optional[str] = None,
+) -> bool:
+    if not isinstance(t, tuple):
+        t = (t,)
+    assert len(t) > 0, "t must have at least one type"
+
+    for i in t:
+        if isinstance(v, i):
+            return True
+
+    if r:
+        if op:
+            raise TypeError(f"can only {op} with {t} not with {type(v)}: {v}")
+        else:
+            raise TypeError(f"expected {t} not {type(v)}: {v}")
+
+    return False
